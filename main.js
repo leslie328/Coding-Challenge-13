46f20ed09b1769bc3cd711336e1907593bfc71d3
Leslie Rodriguez <leslie286@usf.edu>
2024-11-03 21:42:35 -0500

    Fetch product data and display basic information

---
+++ main.js
@@ -0,0 +1,15 @@
+function fetchProducts() {
+    fetch('https://www.course-api.com/javascript-store-products')
+        .then(response => {
+            
+            if (!response.ok) {
+                throw new Error('Network response was not ok: ') 
+            } })
+        .then(data => {
+            displayProducts(data); 
+        })
+        .catch(error => {
+            console.error('Error fetching products:', error);
+            document.getElementById('product-container').innerHTML = '<p>Error loading products. Please try again later.</p>';
+        });
+}
